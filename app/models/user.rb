7485ce59476740f1f3ec5cac9428c71d1a276ac4
simokh <m.simokhouili@gmail.com>
2020-10-04 22:32:36 -0400

    setting up sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :museums
     has_many :mreviews
     has_many :museums, through: :mreviews
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'signup' => "users#new"
+
+
   resources :mreviews
   resources :museums
   resources :users
