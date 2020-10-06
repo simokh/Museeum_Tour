164e4446dfb17b658bd475ec99e784a117c601ee
simokh <m.simokhouili@gmail.com>
2020-10-05 22:18:30 -0400

    signup is complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,19 @@ class UsersController < ApplicationController
     end
 
     def create 
+        # binding.pry
         @user = User.new(user_params)
             if @user.save 
                 session[:user_id] = @user.id 
-                redirect_to :show
-
+                redirect_to @user
+                # binding.pry
             else 
                 render :new 
             end
     end
 
     def show 
+
     end
 
     private 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get 'signup' => "users#new"
+  post 'signup' => "users#create"
+
 
 
   resources :mreviews
