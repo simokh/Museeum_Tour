028c0902455869e95a26fd4219f4026f35775a3d
simokh <m.simokhouili@gmail.com>
2021-01-12 22:42:38 -0500

    refactoring user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
-    def index  
-    end
+    # def index  
+    # end
 
     def new 
         @user = User.new
@@ -20,22 +20,27 @@ class UsersController < ApplicationController
     end
 
     def show 
-        @user = User.find_by_id(session[:user_id])
+        # @user = User.find_by_id(session[:user_id])
+        user 
         redirect_to '/' if !@user
     end
 
     def update
-        @user = User.find_by_id(session[:user_id])
+        # @user = User.find_by_id(session[:user_id])
+        user 
         redirect_to '/' if !@user
     end
 
     private 
 
-    def user_params 
+    def user_params
         params.require(:user).permit(:email, :user_name, :password)
+        # params.require(:user).require(:email, :password).permit(*args)
     end
 
- 
+    def user 
+        @user = User.find_by_id(session[:user_id]
+    end
 
 
 end
