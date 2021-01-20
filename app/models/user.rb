d71788067f67124dff061781f70745a59e860946
simokh <m.simokhouili@gmail.com>
2021-01-20 13:03:09 -0500

    fixed validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-    has_many :museums
+    has_many :museums # creating museum visit 
     has_many :mreviews
-    has_many :museums, through: :mreviews
+    has_many :museums, through: :mreviews # museuems they have reviewed 
     has_secure_password
+    validates :email, presence: true
+    validates :user_name, presence: true
+
 end
